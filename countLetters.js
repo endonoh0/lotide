bf1ebcf69e5d9dc071f319628cdf7967d9f4692c
endonoh0 <endonoho@gmail.com>
2020-05-27 22:35:29 +0000

    Change assertion test

--- countLetters.js
+++ countLetters.js
@@ -30,15 +30,6 @@ const countLetters = function (str) {
     return obj;
 };
 
-assertArraysEqual(countLetters('lighthouse in the house'), {
-    l: 1,
-    i: 2,
-    g: 1,
-    h: 4,
-    t: 2,
-    o: 2,
-    u: 2,
-    s: 2,
-    e: 3,
-    n: 1
-});
+assertArraysEqual(countLetters('lighthouse in the house').l, 1);
+assertArraysEqual(countLetters('vancouver').v, 2);
+assertArraysEqual(countLetters('hello').e, 1);
