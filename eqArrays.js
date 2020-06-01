41d8c122fe5d16badaa025e635c8c2238d671c32
endonoh0 <endonoho@gmail.com>
2020-06-01 16:24:13 +0000

    Export eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,9 @@
-const eqArrays = function (arr1, arr2) { // [2, 3], [2,3]
+const eqArrays = function (arr1, arr2) {
     for (let i = arr1.length; i--;) {
         if (arr1.length !== arr2.length) {
             return false;
         }
         if (arr1[i] !== arr2[i]) {
-            // console.log(arr1[i], arr2[i]);
             if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
                 return eqArrays(arr1[i], arr2[i]);
             } else {
