68936318b3f6c74dbe796424a30729436182c3aa
endonoh0 <endonoho@gmail.com>
2020-06-02 01:50:25 +0000

    Refactor test code

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,15 @@
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+assertObjectsEqual(ab, ba);
+assertObjectsEqual(ab, abc);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+
+assertObjectsEqual(cd, dc);
+assertObjectsEqual(cd, cd2);
